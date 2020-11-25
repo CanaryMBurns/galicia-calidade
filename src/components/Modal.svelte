397d8c53648620ea3ec4d9264286bb4c2e92a883
Sergio Vegas Alonso <sergio.vegas.alonso@gmail.com>
2020-11-26 00:16:59 +0100

    Beta OK.

---
+++ src/components/Modal.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { isModalActive } from './stores.js';
+</script>
+
+<div class="modal {$isModalActive ? 'is-active' : ''}">
+  <div class="modal-background" on:click={isModalActive.update}></div>
+  <div class="modal-card">
+    <header class="modal-card-head">
+      <p class="modal-card-title">Modal title</p>
+      <button class="delete" aria-label="close"></button>
+    </header>
+    <section class="modal-card-body">
+      <!-- Content ... -->
+    </section>
+    <footer class="modal-card-foot">
+      <button class="button is-success">Save changes</button>
+      <button class="button">Cancel</button>
+    </footer>
+  </div>
+</div>
--- src/components/Table.svelte
+++ src/components/Table.svelte
@@ -1,72 +1,166 @@
 <script>
+
   // Data
   const places = [
     {
-      name: 'Vigo',
+      name: 'A Lama',
       icon: 'fa-chess-king',
-      hasVeganFood: true,
-      hasBeach: true
+      patrimonio: true,
+      senderismo: true,
+      museos: false,
+      festas: true
     },
     {
-      name: 'Lugo',
+      name: 'Barro',
       icon: 'fa-chess-knight',
-      hasVeganFood: false,
-      hasBeach: false
+      patrimonio: false,
+      senderismo: true,
+      museos: false,
+      festas: true
     },
     {
-      name: 'Santiago de Compostela',
+      name: 'Campo Lameiro',
       icon: 'fa-chess-pawn',
-      hasVeganFood: true,
-      hasBeach: false
+      patrimonio: true,
+      senderismo: false,
+      museos: false,
+      festas: true
     },
     {
-      name: 'Ourense',
+      name: 'Cerdedo - Cotobade',
       icon: 'fa-chess-queen',
-      hasVeganFood: false,
-      hasBeach: false
+      patrimonio: false,
+      senderismo: true,
+      museos: false,
+      festas: true
     },
     {
-      name: 'A coruña',
+      name: 'Marín',
       icon: 'fa-chess-rook',
-      hasVeganFood: true,
-      hasBeach: true
+      patrimonio: true,
+      senderismo: true,
+      museos: false,
+      festas: true
     },
     {
-      name: 'Ferrol',
+      name: 'Poio',
       icon: 'fa-chess-bishop',
-      hasVeganFood: false,
-      hasBeach: true
+      patrimonio: true,
+      senderismo: false,
+      museos: false,
+      festas: false
+    },
+    {
+      name: 'Ponte Caldelas',
+      icon: 'fa-cheese',
+      patrimonio: false,
+      senderismo: true,
+      museos: false,
+      festas: true
+    },
+    {
+      name: 'Pontevedra',
+      icon: 'fa-pepper-hot',
+      patrimonio: true,
+      senderismo: true,
+      museos: true,
+      festas: true
+    },
+    {
+      name: 'Vilaboa',
+      icon: 'fa-fish',
+      patrimonio: true,
+      senderismo: true,
+      museos: true,
+      festas: true
     }
   ];
 
   // Variables
   let placesFiltered = places;
-  let inputString = '';
-
-  const filterPlaces = () => {
-    placesFiltered = places.filter((e) => { return e.name.toLowerCase().includes(inputString.toLowerCase()); });
+  let isModalActive = false;
+  let searchOptions = {
+    inputString: '',
+    patrimonio: false,
+    senderismo: false,
+    museos: false,
+    festas: false
   }
 
-  const keyUp = () => {
+  const filterPlaces = () => {
+    console.log(searchOptions)
+    placesFiltered = places.filter((e) => {
+      
+      let one = true;
+      let two = true;
+      let three = true;
+      let four = true;
+      let five = true;
+
+      if(!e.name.toLowerCase().includes(searchOptions.inputString.toLowerCase())) {
+        one = false;
+      }
+
+      if(searchOptions.patrimonio && !e.patrimonio) {
+        two = false;
+      }
+
+      if(searchOptions.senderismo && !e.senderismo) {
+        three = false;
+      }
+
+      if(searchOptions.museos && !e.museos) {
+        four = false;
+      }
+
+      if(searchOptions.festas && !e.festas) {
+        five = false;
+      }
+
+      return one && two && three && four && five;
+    });
+  }
+
+  const change = () => {
     filterPlaces();
   };
+
+  filterPlaces();
   
 </script>
 
 <article class="panel is-success">
   <p class="panel-heading">
-    Places
+    Destinos
   </p>
   <div class="panel-block">
     <p class="control has-icons-left">
-      <input bind:value={inputString} class="input is-success" type="text" placeholder="Search by name" on:keyup="{keyUp}">
+      <input bind:value={searchOptions.inputString} class="input is-success" type="text" placeholder="Búsqueda por concello" on:keyup="{change}">
       <span class="icon is-left">
         <i class="fas fa-search" aria-hidden="true"></i>
       </span>
     </p>
+    <div class="checkboxes-wrapper">
+      <label class="checkbox">
+        <input bind:checked={searchOptions.patrimonio} type="checkbox" on:change="{change}">
+        Patrimonio
+      </label>
+      <label class="checkbox">
+        <input  bind:checked={searchOptions.senderismo} type="checkbox" on:change="{change}">
+        Senderismo
+      </label>
+      <label class="checkbox">
+        <input bind:checked={searchOptions.museos} type="checkbox" on:change="{change}">
+        Museos
+      </label>
+      <label class="checkbox">
+        <input bind:checked={searchOptions.festas} type="checkbox" on:change="{change}">
+        Festas
+      </label>
+    </div>
   </div>
   {#each placesFiltered as { name, icon }, i}
-    <a href="#" class="panel-block is-active">
+    <a href="#" class="panel-block is-active" on:click="{isModalActive.update}">
       <span class="panel-icon">
         <i class="fas {icon}" aria-hidden="true"></i>
       </span>
@@ -79,6 +173,25 @@
 article {
   width: 50%;
   background-color: white;
+}
+
+.panel-block {
+  flex-wrap: wrap;
+  width: 100%;
+}
+
+.checkboxes-wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  width: 100%;
+  padding-top: 0.75rem;
+}
+
+.checkbox {
+  width: 50%;
+  padding-top: 0.75rem;
+  padding-left: 0.75rem;
 }
 
 @media (max-width: 900px) {
